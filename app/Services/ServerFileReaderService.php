4fbc68aca4eec0f223a85c6f13f1596a617e0ee0
Weerapat Supap <weerapatsu@gmail.com>
2017-07-04 17:30:37 +0700

    Remove some code

--- app/Services/ServerFileReaderService.php
+++ app/Services/ServerFileReaderService.php
@@ -5,8 +5,6 @@ use Exception;
 
 class ServerFileReaderService
 {
-    private $maxReadFileSize = 30000000;
-
     /**
      * Reads file from server
      *
@@ -16,21 +14,6 @@ class ServerFileReaderService
      */
     public function read(string $filePath) : array
     {
-
-        if (\File::exists($filePath) && filesize($filePath) > $this->maxReadFileSize) {
-            return [
-                'status' => 'error',
-                'message' => 'Log file cannot bigger than 30m',
-            ];
-        }
-
-        if (!is_file($filePath)) {
-            return [
-                'status' => 'error',
-                'message' => 'This file does not exist!',
-            ];
-        }
-
         try {
             $data = file($filePath);
         } catch (Exception $e) {
